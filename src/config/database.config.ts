668ae3750961c59bbb8a54cd0d89a3438be0f8c4
Ivan Podymov <p0dymovivan@yandex.ru>
2025-12-02 15:22:15 +0300

    Fix cors and db_url for connect onto database

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,26 +1,15 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
 export const getDatabaseConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => {
-  const host = configService.get<string>('DB_HOST', 'localhost');
-  const port = configService.get<number>('DB_PORT', 5432);
-  const username = configService.get<string>('DB_USERNAME', 'postgres');
-  const password = configService.get<string>('DB_PASSWORD', 'postgres');
-  const database = configService.get<string>('DB_DATABASE', 'fakegram');
+  const databaseUrl = configService.get<string>('DATABASE_URL');
   const nodeEnv = configService.get<string>('NODE_ENV', 'development');
 
   return {
     type: 'postgres',
-    host,
-    port,
-    username,
-    password,
-    database,
+    url: databaseUrl,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: nodeEnv === 'development',
     logging: nodeEnv === 'development',
