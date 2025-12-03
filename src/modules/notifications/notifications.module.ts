6334f35bebc15518e181d537450559f31fb4d328
Ivan Podymov <p0dymovivan@yandex.ru>
2025-12-03 21:20:07 +0300

    Fixing bug

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 import { Notification } from '../../entities/notification.entity';
 import { User } from '../../entities/user.entity';
+import { getJwtConfig } from '../../config/jwt.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification, User])],
+  imports: [
+    TypeOrmModule.forFeature([Notification, User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: getJwtConfig,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
   exports: [NotificationsService],
